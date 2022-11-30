413a2a4dd655b75e37ac8db878417a455d791126
illiez <illiez@hotmail.com>
2022-11-29 20:45:15 -0500

    heroku node modules

--- heroku.js
+++ heroku.js
@@ -34,6 +34,7 @@ const max = io.of('/max');
 // serve pages from public dir
 // app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
 app.get("/", (req, res) => { res.sendFile(__dirname + '/public/delay.html'); });
 // app.use("/client", clientRoute);
 app.use("/attr", attributionRoute);
