d563a7ce8bd2104176b1e7167cea05d602cdf8e8
illiez <illiez@hotmail.com>
2022-12-06 16:40:17 -0500

    remove nsp count & ID log

--- heroku.js
+++ heroku.js
@@ -56,9 +56,9 @@ for (let i = 0; i < io_nsp.length; i++) {
 }
 
 io.on('connection', (socket) => {
-  console.log(io_nsp.length)
+  // console.log(io_nsp.length)
   for (let i = 0; i < io_nsp.length; i++) {
-    console.log(io_nsp[i].conns)
+    // console.log(io_nsp[i].conns)
     if (io_nsp[i].conns >= 1) {
       socket.emit(`connFull`, `${io_nsp_tag[i]}`)
     }
